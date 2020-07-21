bae55e9d221e4e76b120470c4314b0644e82cf3c
juan <jdmogollonp@gmail.com>
2020-07-21 10:07:07 -0500

    actualizacion método generar test set

--- data_generator/generator.py
+++ data_generator/generator.py
@@ -32,24 +32,26 @@ class Generator(object):
         distintos_letras = list(set(letras))
         declaracion = [x for x in chain(*zip_longest(distintos_letras, [',' for i in range( 0,len(distintos_letras)-1 )])) if x is not None]
         declaracion = ''.join(declaracion)
-        python_funcion = """def f("""+ declaracion +"""):\n\treturn """+operacion+ """ #<end>"""
+        python_funcion = """def f("""+ declaracion +"""):\n\t return """+operacion+ """ #<end>"""
         return(python_funcion)
     
     def generate_training_set(self,size):
         self.training_set = {self.function_generator():1 for i in range(0,size)}
         self.list_training_set = list(self.training_set.keys())
         return self.list_training_set
-        
-    def generate_test_set(self,set_size,cases_set_size):
+
+    def generate_test_set(self,path,set_size,cases_set_size):
+        self.path = path
         test_set = {self.function_generator():1 for i in range(0,set_size)}
         self.test_set = {key:1  for key in test_set.keys() if key not in  self.training_set.keys() }
         list_keys = list(self.test_set.keys())
         self.test_cases = { 'test_cases ' + j :  pd.concat([self.generate_test_case(j) for i in range(0,cases_set_size)])  for  j in   list_keys}
-        for i, key in enumerate(self.test_cases):
-            self.export_to_csv( 'test_cases_' + str(i), self.test_cases[key] )
-        self.list_test_set = list(self.test_set.keys())
-        return self.list_test_set
-    
+        for i, key in enumerate(x.test_cases):
+            self.export_file( self.path,'.csv','test_cases_' + str(i), self.test_cases[key] )
+        self.list_testing_set = list(self.test_set.keys())
+        return self.list_testing_set
+
+
     def generate_test_case(self,function_str):
         function = (function_str.split('def'))[1].split(':')[0]
         operation = (function_str.split('return'))[1].split('#')[0]
